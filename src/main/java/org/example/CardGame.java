b893c584b0de5668dfce3a4f8bd0e9d8553048de
Alisha <ashamsher2002@gmail.com>
2026-05-22 17:00:24 +0100

    sort deck into suits

--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -29,6 +29,22 @@ public class CardGame {
         return deckOfCards.remove(0);
     }
 
+    public ArrayList<Card> sortDeckIntoSuits() {
+        deckOfCards.sort(
+                (card1, card2) -> {
+                    int suitCompare = card1.getSuit().compareTo(card2.getSuit());
+
+                    if (suitCompare == 0) {
+                        return card1.getValue() - card2.getValue();
+                    }
+
+                    return suitCompare;
+                }
+        );
+
+        return deckOfCards;
+    }
+
     public void shuffleDeck () {
         Collections.shuffle(deckOfCards);
     }
